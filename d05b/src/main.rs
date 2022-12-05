9e625a7ea23a1931be61486bf708a5d5c47ad292
Arnaud Bertrand <nadaru@pm.me>
2022-12-05 13:17:35 +0100

    d05

---
+++ d05b/src/main.rs
@@ -0,0 +1,50 @@
+fn main() {
+    let (matrix, instructions) = include_str!("../input").split_once("\n\n").unwrap();
+
+    let mut reversed = matrix.lines().rev();
+
+    let size = (reversed.next().unwrap().len() + 1) / 4;
+
+    // TODO: Check if there's a better way to create matrix
+    let mut stacks: Vec<Vec<char>> = Vec::new();
+    for _i in 0..size {
+        stacks.push(Vec::new())
+    }
+
+    loop {
+        match reversed.next() {
+            None => break,
+            Some(line) => {
+                let chars = line.chars().collect::<Vec<char>>();
+                for i in 0..size {
+                    let &char = chars.get(i * 4 + 1).unwrap();
+                    if char != ' ' {
+                        stacks[i].push(char);
+                    }
+                }
+            }
+        }
+    }
+
+    instructions
+        .lines()
+        .map(|i| i.split_whitespace().collect::<Vec<_>>())
+        .map(|i| {
+            (
+                i[1].parse::<usize>().unwrap(),
+                i[3].parse::<usize>().unwrap(),
+                i[5].parse::<usize>().unwrap(),
+            )
+        })
+        .for_each(|(n, from, to)| {
+            let stack = &mut stacks[from - 1];
+            let mut removed = stack.splice((stack.len() - n).., []).collect::<Vec<_>>();
+            stacks[to - 1].append(&mut removed);
+        });
+
+    let res = stacks
+        .iter()
+        .fold(String::new(), |a, b| a + &b.last().unwrap().to_string());
+
+    println!("{:?}", res)
+}
